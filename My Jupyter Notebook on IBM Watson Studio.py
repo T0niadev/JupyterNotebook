a467e1f69d818fb61a15db974a435ec2549df825
T0niadev <111419388+T0niadev@users.noreply.github.com>
2022-10-23 16:56:52 +0100

    Add files via upload

---
+++ My Jupyter Notebook on IBM Watson Studio.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **ODUBIYI IFEOLUWA**
+# 
+# Data Scientist
+
+# _I'm enthusiastic about data science, especially considering how quickly technology is changing the profession. I enjoy being a part of new technologies and trying out innovative solutions._
+
+# ### My Code performs Addition of two numbers
+
+# In[5]:
+
+
+1 + 4
+
+
+# ### Numbered lists
+# 1. excellent
+# 2. good
+# 
+#    <blockquote cite="https://www.brainyquote.com/authors/erin_cummings">  
+#     <p>  
+#      At the end of the day, you are solely responsible for your success and your failure. And the sooner you realize that, you accept that, and integrate that into your work ethic, you will start being successful. As long as you blame others for the reason you aren't where you want to be, you will always be a failure.  
+#      </p>  
+#    </blockquote>
+#    
+#     <p><s>My car is blue.</s></p> 
+#     
